88d928c426fe37b327fda2001301584d7327696e
Lucas Rodrigues | R. Amaral Advogados <lucas.rodrigues@RAMARAL.LOCAL>
2025-10-07 14:08:38 -0300

    caos

--- poo_caos2.py
+++ poo_caos2.py
@@ -4,7 +4,18 @@ import random
 
 class Academia():
     def __init__(self):
-        
         #1 vai definir os halteres em números pares de 10 a 36
+        #2 criar uma estrutura para abrigar os halteres
+        #3 crie uma função para reiniciar o dia da academia ( organizando os halteres )
 
         pass
+
+    #crie a função para reiniciar o dia 
+    
+    #crie uma função que retorne todos os halteres disponíveis, que podem ser pegos
+
+    #crie uma funlão de pegar os halteres 
+
+    #crie uma função para devolver o halter 
+
+
